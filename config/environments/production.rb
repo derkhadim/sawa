d4b0d7edf6447064ffea20476c1c368977aa0d30
Mouhamadou der <dermouhamadou@gmail.com>
2026-06-22 16:24:10 +0000

    Fix: clear hosts for Railway

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
 
   config.secret_key_base = ENV['SECRET_KEY_BASE']
 
-  # config.hosts << "sawa-production-41b3.up.railway.app"
-  config.hosts << ENV["APP_HOST"] if ENV["APP_HOST"].present?
+  config.hosts.clear
 
 end
