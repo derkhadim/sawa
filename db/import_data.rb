48127fd81823091ca215df98f7e4e703fcbb3a62
Mouhamadou der <dermouhamadou@gmail.com>
2026-06-23 07:04:58 +0000

    fix boolean conversion in import script

--- db/import_data.rb
+++ db/import_data.rb
@@ -2,6 +2,18 @@
 # bundle exec rails runner db/import_data.rb RAILS_ENV=production
 
 sql = File.read(File.join(__dir__, 'import.sql'))
+
+lines = sql.split("\n")
+fixed = lines.map do |line|
+  if line.start_with?("INSERT INTO apartments ")
+    # Fix boolean visible column: 0/1 -> false/true
+    line.sub(/,(0|1)\);$/) { |m| m.sub('0', 'false').sub('1', 'true') }
+  else
+    line
+  end
+end
+sql = fixed.join("\n")
+
 # Remove BEGIN/COMMIT since execute handles transactions
 sql.sub!(/\ABEGIN;\s*/, '')
 sql.sub!(/;\s*COMMIT;\s*\z/, '')
