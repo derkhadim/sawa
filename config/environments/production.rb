df4f610a882155ef1bae9600f5adda89e6b81808
Mouhamadou der <dermouhamadou@gmail.com>
2026-08-06 12:26:58 +0000

    Disable force_ssl for Railway internal healthcheck + allow railway.app hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
 
-  config.force_ssl = true
+  config.force_ssl = false
   config.log_tags = [:request_id]
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
@@ -14,5 +14,5 @@ Rails.application.configure do
   config.secret_key_base = ENV['SECRET_KEY_BASE']
 
   config.hosts << 'sawa-production-41b3.up.railway.app'
-  
+  config.hosts << /.*\.railway\.app/
 end
